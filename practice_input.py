6a5810526deedaf76157fa1115f42520f56976ea
04sdani <shriya@danicorp.com>
2024-05-27 21:38:14 -0400

    This was our second assignment, where I entered my own code to display my input and output skills. I used the print command, variables, and the sep command.

--- practice_input.py
+++ practice_input.py
@@ -11,6 +11,8 @@ def get_favorite_vegetable():
     where X is replaced with the user's favorite vegetable.
     """
     # write your code here.
+    X = input("What is your favorite vegetable? ")
+    print("Interesting! I also love " + X + "!")
 
 
 def get_favorite_number():
@@ -20,6 +22,8 @@ def get_favorite_number():
     where X is replaced with the user's favorite number.
     """
     # write your code here.
+    X = input("What is your favorite number? ")
+    print("Interesting! I also love " + X + "!")
 
 
 def get_name_and_zodiac_sign():
@@ -30,6 +34,9 @@ def get_name_and_zodiac_sign():
     where X and Y are replaced by the user's name and zodiac sign, respectively.
     """
     # write your code here.
+    X = input("What is your name? ")
+    Y = input("What is your zodiac sign? ")
+    print("Interesting! My name is also " + X + ", and I'm also a " + Y + "!")
 
 
 def get_name_and_age():
@@ -40,3 +47,6 @@ def get_name_and_age():
     where X and Y are replaced by the user's name and age, respectively.
     """
     # write your code here.
+    X = input("What is your name? ")
+    Y = input("What is your age? ")
+    print("Interesting! My name is also " + X + ", and I'm also " + Y + " years old!")
